60bf0f99d505d8d5df0a6131c33a05a089bc012d
hpierre74 <pierrehuyghe.pro@gmail.com>
2019-12-07 23:28:12 +0100

    initial commit

---
+++ articles.go
@@ -0,0 +1,147 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/iterator"
+)
+
+// ArticlesType represents the articles collection in the database
+type ArticlesType []map[string]interface{}
+
+// ArticleFieldsType defines the structure of the fields in an article from the articles collection.
+type ArticleFieldsType struct {
+	ID          string  `firestore:"id"`
+	Name        string  `firestore:"name"`
+	Price       float64 `firestore:"price"`
+	Type        string  `firestore:"type"`
+	Year        string  `firestore:"year"`
+	Image       string  `firestore:"image"`
+	Description string  `firestore:"description"`
+}
+
+// DeleteType represents the body expected structure
+type DeleteType struct {
+	ID string `json:"id"`
+}
+
+// ArticleAPI is an HTTP Cloud Function with a request parameter.
+func ArticleAPI(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+
+	conf := &firebase.Config{ProjectID: "gofire-1280e"}
+
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalf("Firestore init: %v", err)
+	}
+	defer client.Close()
+
+	switch method := r.Method; method {
+	case http.MethodGet:
+		getArticles(ctx, client, w)
+	case http.MethodPost:
+		setArticle(ctx, client, w, r)
+	case http.MethodDelete:
+		deleteArticle(ctx, client, w, r)
+	case http.MethodPut:
+		updateArticle(ctx, client, w, r)
+	default:
+		http.Error(w, "UNSUPPORTED METHOD", http.StatusNotFound)
+	}
+
+}
+
+func getArticles(ctx context.Context, client *firestore.Client, w http.ResponseWriter) {
+
+	var articles ArticlesType
+	iter := client.Collection("articles").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			// handle error
+		}
+
+		articles = append(articles, doc.Data())
+	}
+
+	json.NewEncoder(w).Encode(articles)
+}
+
+func setArticle(ctx context.Context, client *firestore.Client, w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	var newArticle ArticleFieldsType
+
+	err = json.Unmarshal(body, &newArticle)
+	if err != nil {
+		panic(err)
+	}
+	_, err = client.Collection("articles").Doc(newArticle.ID).Create(ctx, &newArticle)
+	if err != nil {
+		panic(err)
+	}
+
+	json.NewEncoder(w).Encode(map[string]bool{"create": true})
+}
+
+func deleteArticle(ctx context.Context, client *firestore.Client, w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	var Body DeleteType
+
+	err = json.Unmarshal(body, &Body)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = client.Collection("articles").Doc(Body.ID).Delete(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	json.NewEncoder(w).Encode(map[string]bool{"delete": true})
+}
+
+func updateArticle(ctx context.Context, client *firestore.Client, w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+
+	var Body ArticleFieldsType
+
+	err = json.Unmarshal(body, &Body)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = client.Collection("articles").Doc(Body.ID).Set(ctx, &Body)
+	if err != nil {
+		panic(err)
+	}
+
+	json.NewEncoder(w).Encode(map[string]bool{"update": true})
+}
---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/hpierre74/articles-api
+
+go 1.12
+
+require (
+	cloud.google.com/go v0.49.0 // indirect
+	cloud.google.com/go/bigquery v1.3.0 // indirect
+	cloud.google.com/go/firestore v1.1.0
+	cloud.google.com/go/pubsub v1.1.0 // indirect
+	cloud.google.com/go/storage v1.4.0 // indirect
+	firebase.google.com/go v3.10.0+incompatible
+	github.com/golang/groupcache v0.0.0-20191027212112-611e8accdfc9 // indirect
+	github.com/google/go-cmp v0.3.1 // indirect
+	github.com/jstemmer/go-junit-report v0.9.1 // indirect
+	go.opencensus.io v0.22.2 // indirect
+	golang.org/x/exp v0.0.0-20191129062945-2f5052295587 // indirect
+	golang.org/x/lint v0.0.0-20191125180803-fdd1cda4f05f // indirect
+	golang.org/x/net v0.0.0-20191207000613-e7e4b65ae663 // indirect
+	golang.org/x/oauth2 v0.0.0-20191202225959-858c2ad4c8b6 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20191206220618-eeba5f6aabab // indirect
+	golang.org/x/tools v0.0.0-20191206204035-259af5ff87bd // indirect
+	google.golang.org/api v0.14.0
+	google.golang.org/appengine v1.6.5 // indirect
+	google.golang.org/genproto v0.0.0-20191206224255-0243a4be9c8f // indirect
+	google.golang.org/grpc v1.25.1 // indirect
+)
