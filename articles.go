9d949ad70c17d1710693f954cd3bb7b9c197301f
Pierre Huyghe <pierrehuyghe.pro@gmail.com>
2019-12-08 01:30:33 +0100

    chore(cors): add cors support (#2)

--- articles.go
+++ articles.go
@@ -47,6 +47,18 @@ func ArticleAPI(w http.ResponseWriter, r *http.Request) {
 	}
 	defer client.Close()
 
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
 	switch method := r.Method; method {
 	case http.MethodGet:
 		getArticles(ctx, client, w)
